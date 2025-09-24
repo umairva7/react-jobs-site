ea8bd79bab7f96390f8a108160723679a7aca84e
Umair Imran <umairva7@gmail.com>
2025-09-24 17:50:35 +0500

    Make Sturcture

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Job Listings</h1>
+      <ul>
+        <li>Software Engineer</li>
+        <li>Product Manager</li>
+        <li>Data Scientist</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
