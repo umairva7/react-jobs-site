225b29282deb371b44b4f8535ce1df2d7b7a2da9
Umair Imran <umairva7@gmail.com>
2025-09-26 20:22:38 +0500

    View All Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,16 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Homecards from './components/Homecards.jsx'
 import JobListings from './components/JobListings.jsx'
-
+import ViewAll from './components/ViewAll.jsx'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero title='Become a React Dev' subtitle='Find the React job that fits your skills and needs' />
       <Homecards />
-      <JobListings />     
+      <JobListings /> 
+      <ViewAll />    
 
-   
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
       
     </div>
   )
