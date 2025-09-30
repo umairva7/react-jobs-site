11d758cc4a753c05156e5e6b9070faa55d63b055
Umair Imran <umairva7@gmail.com>
2025-09-30 07:07:37 +0500

    Active Links with NavLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,61 @@
-import React from 'react'
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
-          <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            
-            <Link className="flex  flex-shrink-0 items-center mr-4" to="/index.html">
-              <img
-                className="h-10 w-auto"
-                src={ logo }
-                alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >React Jobs</span
-              >
-            </Link>
+          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            {/* Logo */}
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+              <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                React Jobs
+              </span>
+            </NavLink>
+
+            {/* Links */}
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center px-2 py-2 rounded-md text-white bg-gray-900"
+                      : "flex items-center px-2 py-2 rounded-md text-white hover:bg-gray-900 hover:text-white"
+                  }
                 >
-                <Link
+                  Home
+                </NavLink>
+                <NavLink
                   to="/jobs"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</Link>
-                <Link to="/add-job"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</Link>
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center px-2 py-2 rounded-md text-white bg-gray-900"
+                      : "flex items-center px-2 py-2 rounded-md text-white hover:bg-gray-900 hover:text-white"
+                  }
+                >
+                  Jobs
+                </NavLink>
+                <NavLink
+                  to="/add-job"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center px-2 py-2 rounded-md text-white bg-gray-900"
+                      : "flex items-center px-2 py-2 rounded-md text-white hover:bg-gray-900 hover:text-white"
+                  }
+                >
+                  Add Job
+                </NavLink>
               </div>
             </div>
           </div>
         </div>
       </div>
     </nav>
-
-    
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
